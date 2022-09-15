0cb8f79074b98a39653c67323d7b15c873f01d78
quezia-lima <qfonseca1@gmail.com>
2022-09-15 12:56:04 -0300

    correção do retorno da função countHolidays

--- contaFeriado.js
+++ contaFeriado.js
@@ -22,17 +22,58 @@ async function getHolidays(ano) {
   }
 }
 
-
-function countHolidays(startDate, endDate, year) {
-  getHolidays(year).then(value => {
+async function countHolidays(startDate, endDate, year) {
+  const result = getHolidays(year).then(value => {
     let holidaysCounter = 0;
+
     value.forEach((day) => {
-      if(((Date.parse(day) >= Date.parse(startDate))) && (Date.parse(day) < Date.parse(endDate))) {
-        holidaysCounter += 1;
+      const dayDate = new Date(Date.parse(day));// formato de data
+      // console.log(dayDate);
+      const weekDay = dayDate.getDay() // dia da semana: 0 é domingo, 6 é sábado
+      // console.log(` data do dia ${dayDate}, dia da semana: ${weekDay} `);
+      // let formattedWeekDay = new Date(dayDate);
+      // console.log(formattedWeekDay.getDay());
+      if( weekDay !== 6 && weekDay !== 0) { // exclui feriados nos sábados e domingos
+        if(((Date.parse(day) >= Date.parse(startDate))) && (Date.parse(day) < Date.parse(endDate))) {
+                holidaysCounter += 1;
+                // console.log('entrou')
+        };
       }
-    })
-  console.log(holidaysCounter);
+    });
+  
+  console.log('contador',holidaysCounter);
+  // console.log('contador',holidaysCounter);
+  return holidaysCounter;  
   } );
+  return result;
 }
 
-countHolidays('2022-01-01','2022-04-22', 2022);
+
+// countHolidays('2022-01-01','2022-04-22', 2022)
+
+// async function countHolidays(startDate, endDate, year) {
+//   return getHolidays(year).then(value => {
+//     let holidaysCounter = 0;
+
+//     value.forEach((day) => {
+//       const dayDate = new Date(Date.parse(day));// formato de data
+//       // console.log(dayDate);
+//       const weekDay = dayDate.getDay() // dia da semana: 0 é domingo, 6 é sábado
+//       // console.log(` data do dia ${dayDate}, dia da semana: ${weekDay} `);
+//       // let formattedWeekDay = new Date(dayDate);
+//       // console.log(formattedWeekDay.getDay());
+//       if( weekDay !== 6 && weekDay !== 0) { // exclui feriados nos sábados e domingos
+//         if(((Date.parse(day) >= Date.parse(startDate))) && (Date.parse(day) < Date.parse(endDate))) {
+//                 holidaysCounter += 1;
+//                 // console.log('entrou')
+//         };
+//       }
+//     });
+  
+//   // console.log('contador',holidaysCounter);
+//   // console.log('contador',holidaysCounter);
+//   // return holidaysCounter;  
+//   } );
+// }
+countHolidays('2022-01-01','2022-04-22', 2022).then(v => console.log(v));
+
